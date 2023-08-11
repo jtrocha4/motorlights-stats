cd5d60bd18254577a938f2b92d83234a23b0004d
jtrocha4 <jtrocha04@gmail.com>
2023-08-11 15:48:18 -0500

    mejora en el diseño de la interfaz

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav className='navbar bg-dark border-bottom border-body' data-bs-theme='dark' id='navbar'>
         <div className='container-fluid'>
           <a className='navbar-brand' href='#'>
-            <img className='logo' src='..\src\assets\logo-motorlights.png' alt='' />
+            <img className='logo' src='..\public\logo-motorlights.png' alt='' />
             <span>Motorlights-Stats</span>
           </a>
         </div>
