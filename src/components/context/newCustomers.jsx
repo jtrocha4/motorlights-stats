8d986681c262f3f1a9d89f3886422d389c7208da
jtrocha4 <jtrocha04@gmail.com>
2023-10-05 15:31:28 -0500

    feat(context): implementacion de las funcionalidades extractId y findNewClients en el contexto newCustomers

--- src/components/context/newCustomers.jsx
+++ src/components/context/newCustomers.jsx
@@ -4,10 +4,43 @@ export const NewCustomerContext = createContext()
 
 export const NewCustomerProvider = ({ children }) => {
   const [excelDataNewCustomers, setExcelDataNewCustomers] = useState([])
+  const [dataNewCustomers, setDataNewCustomers] = useState([])
+  const [customersBySeller, setCustomersBySeller] = useState([])
+
+  const extractId = (array) => {
+    const regex = /\d+/
+    const id = array.map(el => el.match(regex)[0])
+    return id
+  }
+
+  const findNewClients = (dataSaleItem = [], dataNewCustomers = []) => {
+    const customersBySeller = {}
+    dataSaleItem.forEach(element => {
+      dataNewCustomers.forEach(newCustomer => {
+        const customerId = newCustomer.id
+        const elementCustomerId = extractId(element.clientes)
+        if (elementCustomerId.includes(customerId)) {
+          const sellerName = element.vendedor
+          if (customersBySeller[sellerName]) {
+            customersBySeller[sellerName]++
+          } else {
+            customersBySeller[sellerName] = 1
+          }
+        }
+      })
+    })
+    setCustomersBySeller(customersBySeller)
+  }
 
   return (
     <NewCustomerContext.Provider value={{
-      excelDataNewCustomers, setExcelDataNewCustomers
+      excelDataNewCustomers,
+      setExcelDataNewCustomers,
+      dataNewCustomers,
+      setDataNewCustomers,
+      customersBySeller,
+      setCustomersBySeller,
+      findNewClients
     }}
     >
       {children}
