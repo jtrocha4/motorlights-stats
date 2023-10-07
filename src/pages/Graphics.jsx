e42328e923ae6b2adcaaa9f0bc62c2bd771b307a
jtrocha4 <jtrocha04@gmail.com>
2023-10-07 11:00:57 -0500

    update(pages): actualizacion del nombre de la variable extractDateFromDate en el componente Graphics

--- src/pages/Graphics.jsx
+++ src/pages/Graphics.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SimpleBarCharts from '../components/charts/SimpleBarCharts'
 
-const Graphics = ({ dataset, dateData }) => {
+const Graphics = ({ dataset, extractDateFromData }) => {
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Graficos</h2>
         <div className='chart-container'>
-          <SimpleBarCharts dataset={dataset} dateData={dateData} />
+          <SimpleBarCharts dataset={dataset} extractDateFromData={extractDateFromData} />
         </div>
       </div>
     </div>
