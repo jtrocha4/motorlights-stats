f9348a7781c237ed08d28278a865806fd59092a6
jtrocha4 <jtrocha04@gmail.com>
2023-10-07 10:45:45 -0500

    update(context): estados costReportName

--- src/components/context/dateFile.jsx
+++ src/components/context/dateFile.jsx
@@ -3,18 +3,26 @@ import { createContext, useState } from 'react'
 export const DateContext = createContext()
 
 export const DateProvider = ({ children }) => {
+  const [costReportName, setCostReportName] = useState([])
+
   const [dateCostFile, setDateCostFile] = useState([])
   const [dateCollectionFile, setDateCollectionFile] = useState([])
   const [dateAuxiliaryBookFile, setDateAuxiliaryBookFile] = useState([])
+  const [dateSaleItemFile, setDateSaleItemFile] = useState([])
 
   return (
     <DateContext.Provider value={{
+      costReportName,
+      setCostReportName,
+
       dateCostFile,
       setDateCostFile,
       dateCollectionFile,
       setDateCollectionFile,
       dateAuxiliaryBookFile,
-      setDateAuxiliaryBookFile
+      setDateAuxiliaryBookFile,
+      dateSaleItemFile,
+      setDateSaleItemFile
     }}
     >
       {children}
