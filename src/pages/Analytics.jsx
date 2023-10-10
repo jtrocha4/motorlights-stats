de6214db467d7bcaff331e540f2d1026b8f120ed
jtrocha4 <jtrocha04@gmail.com>
2023-10-10 14:00:07 -0500

    feat(pages): Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import TableAnalytics from '../components/TableAnalytics'
+import { DataContext } from '../components/context/data'
 
-const Analytics = () => {
+const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed }) => {
+  const { data } = useContext(DataContext)
+
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Analitica de datos </h2>
+        <div>
+          <TableAnalytics data={data} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} />
+        </div>
       </div>
     </div>
   )
