75a6d445a450333d2e6bc167d9dbe28c27f15016
jtrocha4 <jtrocha04@gmail.com>
2023-10-12 09:24:08 -0500

    feat(components): uso de los estados del ThirdPartiesContext, implementacion de la funcionalidad extractIdNumber, extractUniqueThirdParties, extractSalesFromData y renderizado de elementos en la table

--- src/components/TableAnalytics.jsx
+++ src/components/TableAnalytics.jsx
@@ -1,33 +1,70 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { DataContext } from './context/data'
+import { ThirdPartiesContext } from './context/thirdParties'
 
 const TableAnalytics = ({ data, convertExcelDateToReadable, currencyFormat, toFixed }) => {
   const { excelDataCost } = useContext(DataContext)
+  const { excelDataThirdParties, thirdPartiesData } = useContext(ThirdPartiesContext)
   const [salesData, setSalesData] = useState([])
+  const [uniqueCustomers, setUniqueCustomers] = useState([])
 
-  const extractSalesFromData = (dataArray = []) => {
-    const sales = dataArray.map(element => {
-      if (element.venta !== undefined) {
-        return element.venta.map(el => {
-          const { ...restOfData } = el
-          return {
-            ...restOfData,
-            vendedor: element.vendedor,
-            unitarioVentas: toFixed(el.unitarioVentas),
-            ventas: toFixed(el.ventas)
-          }
-        })
+  const extractIdNumber = (string) => {
+    const regex = /\d+/
+    const id = string.match(regex)
+    if (id !== null) {
+      return id[0]
+    } else {
+      console.error('the string must contain numbers')
+    }
+  }
+
+  const extractProductText = (string) => {
+    const regex = /^\d+\s*(.+)/
+    const product = string.match(regex)
+    return product[1]
+  }
+
+  const extractUniqueThirdParties = (dataThirdParties = []) => {
+    const uniqueCustomers = []
+    const uniqueCustomerNames = new Set()
+
+    dataThirdParties.forEach(customer => {
+      if (!uniqueCustomerNames.has(customer.nombre)) {
+        uniqueCustomerNames.add(customer.nombre)
+        uniqueCustomers.push(customer)
       }
-      return []
     })
+    setUniqueCustomers(uniqueCustomers)
+  }
+
+  const extractSalesFromData = (dataArray = [], dataThirdParties = []) => {
+    const sales = dataThirdParties.flatMap(customer =>
+      dataArray.filter(element => element.venta !== undefined)
+        .map(element =>
+          element.venta.filter(el => el.cliente === customer.nombre)
+            .map(el => ({
+              ...el,
+              idTercero: extractIdNumber(customer.id),
+              ciudadTercero: customer.ciudad,
+              vendedor: element.vendedor,
+              producto: extractProductText(el.codigoInventario),
+              idProducto: extractIdNumber(el.codigoInventario),
+              unitarioVentas: toFixed(el.unitarioVentas),
+              ventas: toFixed(el.ventas)
+            }))
+        )
+        .flat()
+    )
     setSalesData(sales)
   }
 
-  console.log(salesData)
+  useEffect(() => {
+    extractUniqueThirdParties(thirdPartiesData)
+  }, [excelDataCost, excelDataThirdParties])
 
   useEffect(() => {
-    extractSalesFromData(data)
-  }, [excelDataCost])
+    extractSalesFromData(data, uniqueCustomers)
+  }, [uniqueCustomers])
 
   return (
     <div>
@@ -38,7 +75,10 @@ const TableAnalytics = ({ data, convertExcelDateToReadable, currencyFormat, toFi
             <th>Documento</th>
             <th>Fecha</th>
             <th>Tercero</th>
+            <th>ID Tercero</th>
+            <th>Municipio</th>
             <th>Producto</th>
+            <th>ID Producto</th>
             <th>Unidades</th>
             <th>Valor unitario</th>
             <th>Venta bruta</th>
@@ -47,23 +87,23 @@ const TableAnalytics = ({ data, convertExcelDateToReadable, currencyFormat, toFi
         <tbody className='table-group-divider'>
           {
             (data.length === 0)
-              ? (<tr><td colSpan={8} className='text-center'>No hay datos para mostrar, por favor cargue todos los informes</td></tr>)
+              ? (<tr><td colSpan={11} className='text-center'>No hay datos para mostrar, por favor cargue todos los informes</td></tr>)
               : (
-                // <tr><td colSpan={4} className='text-center'>Hay datos</td></tr>
-                  salesData.flatMap((subArray, index) =>
-                    subArray.map((el, subIndex) => (
-                      <tr key={index * 1000 + subIndex}>
-                        <td> {el.vendedor}</td>
-                        <td>{el.doc}</td>
-                        <td>{convertExcelDateToReadable(el.fecha)}</td>
-                        <td>{el.cliente}</td>
-                        <td>{el.codigoInventario}</td>
-                        <td>{el.unidades}</td>
-                        <td>{currencyFormat(el.unitarioVentas)}</td>
-                        <td>{currencyFormat(el.ventas)}</td>
-                      </tr>
-                    ))
-                  )
+                  salesData.map((el, index) => (
+                    <tr key={index}>
+                      <td> {el.vendedor}</td>
+                      <td>{el.doc}</td>
+                      <td>{convertExcelDateToReadable(el.fecha)}</td>
+                      <td>{el.cliente}</td>
+                      <td>{el.idTercero}</td>
+                      <td>{el.ciudadTercero}</td>
+                      <td>{el.producto}</td>
+                      <td>{el.idProducto}</td>
+                      <td>{el.unidades}</td>
+                      <td>{currencyFormat(el.unitarioVentas)}</td>
+                      <td>{currencyFormat(el.ventas)}</td>
+                    </tr>
+                  ))
                 )
           }
         </tbody>
