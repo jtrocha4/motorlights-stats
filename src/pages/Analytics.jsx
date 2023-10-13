5f8507eac60408b22ddef2dd3781daa9f3b7fce9
jtrocha4 <jtrocha04@gmail.com>
2023-10-13 16:04:30 -0500

    update(pages): Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import TableAnalytics from '../components/TableAnalytics'
 import { DataContext } from '../components/context/data'
 
-const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed }) => {
+const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department }) => {
   const { data } = useContext(DataContext)
 
   return (
@@ -10,7 +10,7 @@ const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed }) => {
       <div className='container-fluid'>
         <h2>Analitica de datos </h2>
         <div>
-          <TableAnalytics data={data} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} />
+          <TableAnalytics data={data} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} />
         </div>
       </div>
     </div>
