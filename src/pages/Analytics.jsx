f3fef0857ac5f2143a399b08ee61b830c0905e33
jtrocha4 <jtrocha04@gmail.com>
2023-10-18 14:20:44 -0500

    feat(pages): implementacion del componente ButtonDownloadAnalytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -2,8 +2,9 @@ import React, { useContext } from 'react'
 import TableAnalytics from '../components/TableAnalytics'
 import { SaleItemContext } from '../components/context/saleItem'
 import { DataContext } from '../components/context/data'
+import ButtonDownloadAnalytics from '../components/ButtonDownloadAnalytics'
 
-const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department, capitalizeWords }) => {
+const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department, capitalizeWords, extractText, extractIdNumber }) => {
   const { dataSaleItem } = useContext(SaleItemContext)
   const { dateExcel } = useContext(DataContext)
 
@@ -13,10 +14,12 @@ const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, depart
         <h2>Analitica de datos </h2>
         <p>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</p>
         <div className='mt-4'>
-          <div className='d-grid gap-2 d-md-flex justify-content-md-end mb-2' />
+          <div className='d-grid gap-2 d-md-flex justify-content-md-end mb-2'>
+            <ButtonDownloadAnalytics title='Descargar informe' background='success' />
+          </div>
         </div>
         <div className='mt-2'>
-          <TableAnalytics dataSaleItem={dataSaleItem} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} capitalizeWords={capitalizeWords} />
+          <TableAnalytics dataSaleItem={dataSaleItem} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} capitalizeWords={capitalizeWords} extractIdNumber={extractIdNumber} extractText={extractText} />
         </div>
       </div>
     </div>
