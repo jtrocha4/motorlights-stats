f5d51a853c3436d78788469a9c004c7c0fbecbf6
jtrocha4 <jtrocha04@gmail.com>
2023-10-17 21:15:26 -0500

    update(pages): actualizacion del contexto y drilling de la funcion capitalizeWords en el componente Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react'
 import TableAnalytics from '../components/TableAnalytics'
-import { DataContext } from '../components/context/data'
+import { SaleItemContext } from '../components/context/saleItem'
 
-const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department }) => {
-  const { data } = useContext(DataContext)
+const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department, capitalizeWords }) => {
+  const { dataSaleItem } = useContext(SaleItemContext)
 
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Analitica de datos </h2>
         <div>
-          <TableAnalytics data={data} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} />
+          <TableAnalytics dataSaleItem={dataSaleItem} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} capitalizeWords={capitalizeWords} />
         </div>
       </div>
     </div>
