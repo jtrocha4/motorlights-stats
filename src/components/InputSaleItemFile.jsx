cca0aa76df5fd9dd5f84abe0440e6261f56cdc73
jtrocha4 <jtrocha04@gmail.com>
2023-10-18 20:27:44 -0500

    feat(components): implementacion de la funcion productCategory, addCategory, extractSalesFromData y manejo del useEffect. Componente InputSaleItemFile

--- src/components/InputSaleItemFile.jsx
+++ src/components/InputSaleItemFile.jsx
@@ -3,10 +3,13 @@ import * as XLSX from 'xlsx'
 import { saleItemFileToModel } from '../mappers'
 import { SaleItemContext } from './context/saleItem'
 import { ReportDetailsContext } from './context/reportDetails'
+import { ThirdPartiesContext } from './context/thirdParties'
 
-const InputSaleItemFile = ({ label }) => {
-  const { excelDataSaleItem, setExcelDataSaleItem, setSellersCustomers, setDataSaleItem } = useContext(SaleItemContext)
+const InputSaleItemFile = ({ label, convertExcelDateToReadable, extractIdNumber, extractText }) => {
+  const { excelDataSaleItem, setExcelDataSaleItem, setSellersCustomers, setDataSaleItem, dataSaleItem, setSellerSalesData } = useContext(SaleItemContext)
   const { setSalesItemsReportName } = useContext(ReportDetailsContext)
+
+  const { customerData } = useContext(ThirdPartiesContext)
 
   const handleReadSaleItemFile = (event) => {
     const file = event.target.files[0]
@@ -106,11 +109,74 @@ const InputSaleItemFile = ({ label }) => {
     setDataSaleItem(dataSalesItems)
   }
 
+  const productCategory = (product) => {
+    if (product.includes('Alarma')) return 'Alarmas'
+    if (product.includes('Bombillo Farola')) return 'Bombillos de Faro'
+    if (product.includes('Bombillo Stop') || product.includes('Bombilllo Stop')) return 'Bombillos de Stop'
+    if (product.includes('Bombillo Direccional') || product.includes('Direccionales') || product.includes('Bombillo Piloto') || product.includes('Direccional')) return 'Bombillos de Direccional'
+
+    if (product.includes('Bombillo Techo')) return 'Bombillos de Techo'
+    if (product.includes('Lagrima')) return 'Lagrimas'
+    if (product.includes('Exploradora') || product.includes('Explorador')) return 'Exploradoras'
+
+    if (product.includes('Switche') || product.includes('Terminal') || product.includes('Socket') || product.includes('Fusible') || product.includes('Flasher')) { return 'Electricos' }
+
+    if (product.includes('Protector') || product.includes('Maniguetas')) return 'Protectores'
+    if (product.includes('Tornillo')) return 'Tornillos'
+    if (product.includes('Modulo Led') || product.includes('Modulo')) return 'Modulos LED'
+    if (product.includes('Cinta Led') || product.includes('Modulo') || product.includes('Amarra') || product.includes('Luz Maletero') || product.includes('Ojo')) return 'Otros'
+    if (product.includes('Guardabarros')) return 'Guardabarros'
+    if (product.includes('Lubricante')) return 'Linea de Mtto'
+    if (product.includes('Guardapolvo')) return 'Guardapolvos'
+
+    return 'Sin Categoria'
+  }
+
+  const addCategory = (salesData = []) => {
+    const categorizedSalesData = salesData.map(el => {
+      const category = productCategory(el.producto)
+      return {
+        ...el,
+        categoriaProducto: category
+      }
+    })
+    return categorizedSalesData
+  }
+
+  const extractSalesFromData = (dataArray = [], dataThirdParties = []) => {
+    const sales = dataThirdParties.flatMap(customer =>
+      dataArray.filter(element => element.itemsVendidos !== undefined)
+        .map(element =>
+          element.itemsVendidos.filter(el => {
+            const idCustomer = extractIdNumber(el.cliente)
+            return idCustomer === extractIdNumber(customer.id)
+          }).map(({ fecha, cliente, descripcion, cantidad, ...restOfData }) => ({
+            ...restOfData,
+            fecha: convertExcelDateToReadable(fecha),
+            cliente: extractText(cliente),
+            idCliente: extractIdNumber(cliente),
+            ciudadCliente: customer.ciudad,
+            departamentoCliente: customer.departamento,
+            idProducto: extractIdNumber(descripcion),
+            producto: extractText(descripcion),
+            unidadesProducto: cantidad
+          }))
+        )
+        .flat()
+    )
+    const salesWithCategory = addCategory(sales)
+    setSellerSalesData(salesWithCategory)
+  }
+
   useEffect(() => {
     extractCustomersFromSeller(formattedDataSaleItem)
     extractDataSaleItems(formattedDataSaleItem)
     setSalesItemsReportName(reportName)
   }, [excelDataSaleItem])
+
+  useEffect(() => {
+    extractSalesFromData(dataSaleItem, customerData)
+  }, [customerData])
 
   return (
     <>
