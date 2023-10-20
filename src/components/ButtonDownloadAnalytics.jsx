6f7aa0911bfa3ac84b7b8cf7f6910172ead0e47c
jtrocha4 <jtrocha04@gmail.com>
2023-10-19 19:25:09 -0500

    feat(components): implementacion de la funcion handleDownload en el componente ButtonDownloadAnalytics

--- src/components/ButtonDownloadAnalytics.jsx
+++ src/components/ButtonDownloadAnalytics.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import XLSX from 'xlsx-js-style'
+import excelStyles from '../styles/excelStyles'
 
-const ButtonDownloadAnalytics = ({ title, background = 'primary' }) => {
+const ButtonDownloadAnalytics = ({ title, background = 'primary', sellerSalesData }) => {
   const handleDownload = (event) => {
-    console.log('Descargando...')
+    const tableHeaders = []
+    tableHeaders.push([
+      { v: 'Vendedor', s: excelStyles.headerGrayStyle },
+      { v: 'Documento', s: excelStyles.headerGrayStyle },
+      { v: 'Fecha', s: excelStyles.headerGrayStyle },
+      { v: 'ID Tercero', s: excelStyles.headerGrayStyle },
+      { v: 'Tercero', s: excelStyles.headerGrayStyle },
+      { v: 'Municipio', s: excelStyles.headerGrayStyle },
+      { v: 'Departamento', s: excelStyles.headerGrayStyle },
+      { v: 'ID Producto', s: excelStyles.headerGrayStyle },
+      { v: 'Producto', s: excelStyles.headerGrayStyle },
+      { v: 'Categoria', s: excelStyles.headerGrayStyle },
+      { v: 'Unidades', s: excelStyles.headerGrayStyle },
+      { v: 'Valor Unitario', s: excelStyles.headerGrayStyle },
+      { v: 'Venta Bruta', s: excelStyles.headerGrayStyle },
+      { v: 'Descuento', s: excelStyles.headerGrayStyle },
+      { v: 'Venta Nea', s: excelStyles.headerGrayStyle },
+      { v: 'IVA', s: excelStyles.headerGrayStyle },
+      { v: 'Valor Total', s: excelStyles.headerGrayStyle }
+    ])
+
+    const wsData = sellerSalesData.map(row => ({
+      vendedor: { v: row.vendedor, s: excelStyles.whiteStyleRow },
+      doc: { v: row.doc, s: excelStyles.whiteStyleRow },
+      fecha: { v: row.fecha, s: excelStyles.whiteStyleRow },
+      idCliente: { v: row.idCliente, s: excelStyles.whiteStyleRow, t: 'n' },
+      cliente: { v: row.cliente, s: excelStyles.whiteStyleRow },
+      ciudadCliente: { v: row.ciudadCliente, s: excelStyles.whiteStyleRow },
+      departamentoCliente: { v: row.departamentoCliente, s: excelStyles.whiteStyleRow },
+      idProducto: { v: row.idProducto, s: excelStyles.whiteStyleRow, t: 'n' },
+      producto: { v: row.producto, s: excelStyles.whiteStyleRow },
+      categoriaProducto: { v: row.categoriaProducto, s: excelStyles.whiteStyleRow },
+      unidadesProducto: { v: row.unidadesProducto, s: excelStyles.whiteStyleRow, t: 'n' },
+      valorUnitario: { v: row.valorUnitario, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' },
+      ventaBruta: { v: row.ventaBruta, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' },
+      descuento: { v: row.descuento, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' },
+      ventaNeta: { v: row.ventaNeta, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' },
+      iva: { v: row.iva, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' },
+      valorTotal: { v: row.valorTotal, s: excelStyles.whiteStyleRowCurrencyFormat, t: 'n' }
+    }))
+
+    const worksheet = XLSX.utils.json_to_sheet(wsData)
+
+    const workbook = XLSX.utils.book_new()
+    const sheetName = 'Datos'
+
+    // Tamaño de las columnas
+    worksheet['!cols'] = []
+
+    const sellerColumnSize = wsData.reduce((w, r) => Math.max(w, r.vendedor.v.length), 10)
+    const customerColumnSize = wsData.reduce((w, r) => Math.max(w, r.cliente.v.length), 10)
+    const docColumnSize = wsData.reduce((w, r) => Math.max(w, r.doc.v.length), 10)
+    const deptColumnSize = wsData.reduce((w, r) => Math.max(w, r.departamentoCliente.v.length), 10)
+    const idProductColumnSize = wsData.reduce((w, r) => Math.max(w, r.idProducto.v.length), 10)
+    const productColumnSize = wsData.reduce((w, r) => Math.max(w, r.producto.v.length), 10)
+    const productCategoryColumnSize = wsData.reduce((w, r) => Math.max(w, r.categoriaProducto.v.length), 10)
+
+    const currencyFormatColumnSize = 15
+
+    worksheet['!cols'][0] = { wch: sellerColumnSize }
+
+    worksheet['!cols'][1] = { wch: docColumnSize }
+    worksheet['!cols'][4] = { wch: customerColumnSize }
+    worksheet['!cols'][6] = { wch: deptColumnSize + 2 }
+    worksheet['!cols'][7] = { wch: idProductColumnSize }
+    worksheet['!cols'][8] = { wch: productColumnSize }
+    worksheet['!cols'][9] = { wch: productCategoryColumnSize }
+    worksheet['!cols'][11] = { wch: currencyFormatColumnSize }
+    worksheet['!cols'][12] = { wch: currencyFormatColumnSize }
+    worksheet['!cols'][13] = { wch: currencyFormatColumnSize }
+    worksheet['!cols'][14] = { wch: currencyFormatColumnSize }
+    worksheet['!cols'][15] = { wch: currencyFormatColumnSize }
+    worksheet['!cols'][16] = { wch: currencyFormatColumnSize }
+
+    worksheet['!autofilter'] = { ref: 'A1:Q1' }
+
+    XLSX.utils.book_append_sheet(workbook, worksheet, sheetName)
+    XLSX.utils.sheet_add_aoa(worksheet, tableHeaders, { origin: 'A1' })
+
+    const excelFileName = 'Informe Ventas.xlsx'
+    XLSX.writeFile(workbook, excelFileName)
   }
 
   return (
