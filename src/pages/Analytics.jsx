71a9dda83e55a6e46991c70b76a1603d0a3afee6
jtrocha4 <jtrocha04@gmail.com>
2023-10-19 19:22:37 -0500

    feat(pages): drilling de parametros en los componentes de Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -4,7 +4,7 @@ import { SaleItemContext } from '../components/context/saleItem'
 import { DataContext } from '../components/context/data'
 import ButtonDownloadAnalytics from '../components/ButtonDownloadAnalytics'
 
-const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, department, capitalizeWords, extractText, extractIdNumber }) => {
+const Analytics = ({ convertExcelDateToReadable, currencyFormat }) => {
   const { dateExcel } = useContext(DataContext)
   const { sellerSalesData } = useContext(SaleItemContext)
 
@@ -15,11 +15,11 @@ const Analytics = ({ convertExcelDateToReadable, currencyFormat, toFixed, depart
         <p>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</p>
         <div className='mt-4'>
           <div className='d-grid gap-2 d-md-flex justify-content-md-end mb-2'>
-            <ButtonDownloadAnalytics title='Descargar informe' background='success' />
+            <ButtonDownloadAnalytics title='Descargar informe' background='success' sellerSalesData={sellerSalesData} />
           </div>
         </div>
         <div className='mt-2'>
-          <TableAnalytics sellerSalesData={sellerSalesData} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} toFixed={toFixed} department={department} capitalizeWords={capitalizeWords} extractIdNumber={extractIdNumber} extractText={extractText} />
+          <TableAnalytics sellerSalesData={sellerSalesData} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} />
         </div>
       </div>
     </div>
