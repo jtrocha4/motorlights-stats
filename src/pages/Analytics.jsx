244fdb81591883144fe75384a24aca0842c41966
jtrocha4 <jtrocha04@gmail.com>
2023-10-30 16:57:03 -0500

    update(pages): componente Analytics

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -11,16 +11,16 @@ const Analytics = ({ convertExcelDateToReadable, currencyFormat }) => {
   return (
     <div className='flex'>
       <div className='container-fluid'>
-        <h2>Analitica de datos </h2>
+        <h2>Analitica de datos</h2>
         <p>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</p>
-        <div className='mt-4'>
+        <section className='mt-4'>
           <div className='d-grid gap-2 d-md-flex justify-content-md-end mb-2'>
             <ButtonDownloadAnalytics title='Descargar informe' background='success' sellerSalesData={sellerSalesData} />
           </div>
-        </div>
-        <div className='mt-2'>
+        </section>
+        <section className='mt-2 table-responsive'>
           <TableAnalytics sellerSalesData={sellerSalesData} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} />
-        </div>
+        </section>
       </div>
     </div>
   )
