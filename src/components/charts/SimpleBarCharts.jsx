f91e3c65dc74ab6b8da7bc9997f8569c144d55ec
jtrocha4 <jtrocha04@gmail.com>
2023-10-30 17:12:46 -0500

    update(components/charts): SimpleBarCharts

--- src/components/charts/SimpleBarCharts.jsx
+++ src/components/charts/SimpleBarCharts.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-undef */
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Bar, CartesianGrid, ComposedChart, LabelList, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import ButtonDownloadImg from '../ButtonDownloadImg'
 import Calendar from '../Calendar'
 
-const SimpleBarCharts = ({ dataset, extractDateFromData }) => {
+const SimpleBarCharts = ({ sellerPerformance, extractDateFromData }) => {
+  const [screenSize, setScreenSize] = useState(0)
+
   const localData = JSON.parse(localStorage.getItem('data')) || {}
   const localDateData = JSON.parse(localStorage.getItem('dateData')) || {}
 
@@ -15,10 +17,10 @@ const SimpleBarCharts = ({ dataset, extractDateFromData }) => {
     return lengthOfObject
   }
 
-  if (getLengthOfObject(localDateData) === 0 && extractDateFromData(dataset)) {
-    porcentajeDiasTranscurridos = extractDateFromData(dataset).porcentajeDiasTranscurridos
-    dia = extractDateFromData(dataset).dia
-    mes = extractDateFromData(dataset).mes
+  if (getLengthOfObject(localDateData) === 0 && extractDateFromData(sellerPerformance)) {
+    porcentajeDiasTranscurridos = extractDateFromData(sellerPerformance).porcentajeDiasTranscurridos
+    dia = extractDateFromData(sellerPerformance).dia
+    mes = extractDateFromData(sellerPerformance).mes
   } else {
     porcentajeDiasTranscurridos = localDateData.porcentajeDiasTranscurridos
     dia = localDateData.dia
@@ -28,14 +30,24 @@ const SimpleBarCharts = ({ dataset, extractDateFromData }) => {
   const percentageData = []
 
   const splitName = (name) => {
-    const nameChain = name.split(' ')
-    const firstAndMiddleName = `${nameChain[0]} ${nameChain[1]}`
-    return firstAndMiddleName
+    if (name !== undefined) {
+      const nameChain = name.split(' ')
+      const firstAndMiddleName = `${nameChain[0]} ${nameChain[1]}`
+      return firstAndMiddleName
+    }
   }
 
   const addDataToPercentageData = (dataArray = [], percentageArray = []) => {
-    dataArray.forEach(({ vendedor, porcentajeVentas, porcentajeRecaudo }) => {
-      const firstAndMiddleName = splitName(vendedor)
+    dataArray.forEach(({ vendedor, idVendedor, porcentajeVentas, porcentajeRecaudo }) => {
+      let firstAndMiddleName = ''
+      if (vendedor) {
+        firstAndMiddleName = splitName(vendedor)
+      } else {
+        if (idVendedor) {
+          firstAndMiddleName = splitName(idVendedor.nombre)
+        }
+      }
+
       percentageArray.push({
         vendedor: firstAndMiddleName,
         porcentajeVentas,
@@ -46,7 +58,7 @@ const SimpleBarCharts = ({ dataset, extractDateFromData }) => {
   }
 
   if (localData.length === 0) {
-    addDataToPercentageData(dataset, percentageData)
+    addDataToPercentageData(sellerPerformance, percentageData)
   } else {
     addDataToPercentageData(localData, percentageData)
   }
@@ -63,36 +75,47 @@ const SimpleBarCharts = ({ dataset, extractDateFromData }) => {
 
   const containerRef = useRef(null)
 
+  const calculateScreenSize = () => {
+    const screenSize = screen.width - 400
+    setScreenSize(screenSize)
+  }
+
+  useEffect(() => {
+    calculateScreenSize()
+  }, [])
+
   return (
-    <div>
+    <>
       <h4 className='text-center'>{(dia !== undefined && mes !== undefined) ? (`Como Vamos ${dia} ${mes}`) : 'Como Vamos'}</h4>
-      <div className='charts-button-group'>
+      <section className='charts-button-group'>
         <Calendar />
-        <ButtonDownloadImg title='Descargar grafica' containerRef={containerRef} date={`${dia} ${mes}`} />
-      </div>
-      <div ref={containerRef}>
-        <ResponsiveContainer width='100%' aspect={3}>
-          <ComposedChart data={leakedData} width={500} height={300} margin={{ top: 20, right: 0, left: 0, bottom: 0 }}>
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='vendedor' tick={{ fill: '#7d7a79' }} />
-            <YAxis tick={{ fill: '#7d7a79' }} tickFormatter={customTooltipFormatter} />
-            <Tooltip formatter={customTooltipFormatter} />
-            <Legend verticalAlign='top' height={36} formatter={customLegendFormatter} wrapperStyle={{ color: '#7d7a79' }} />
-            <Bar name='% Ventas' dataKey='porcentajeVentas' fill='#4673bf'>
-              <LabelList dataKey='porcentajeVentas' position='top' fill='#7d7a79' formatter={customTooltipFormatter} />
-            </Bar>
+        <ButtonDownloadImg title='Descargar grafica' screenSize={screenSize} containerRef={containerRef} date={`${dia} ${mes}`} />
+      </section>
+      <section>
+        <div ref={containerRef} className='simple-bar-charts' id='simple-bar-charts'>
+          <ResponsiveContainer width={screenSize} aspect={3}>
+            <ComposedChart data={leakedData} width={500} height={300} margin={{ top: 20, right: 0, left: 0, bottom: 0 }}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis dataKey='vendedor' tick={{ fill: '#7d7a79' }} />
+              <YAxis tick={{ fill: '#7d7a79' }} tickFormatter={customTooltipFormatter} />
+              <Tooltip formatter={customTooltipFormatter} />
+              <Legend verticalAlign='top' height={36} formatter={customLegendFormatter} wrapperStyle={{ color: '#7d7a79' }} />
+              <Bar name='% Ventas' dataKey='porcentajeVentas' fill='#4673bf'>
+                <LabelList dataKey='porcentajeVentas' position='top' fill='#7d7a79' formatter={customTooltipFormatter} />
+              </Bar>
 
-            <Bar name='% Recaudo' dataKey='porcentajeRecaudo' fill='#fafa0c' color='black'>
-              <LabelList dataKey='porcentajeRecaudo' position='top' fill='#7d7a79' formatter={customTooltipFormatter} />
-            </Bar>
+              <Bar name='% Recaudo' dataKey='porcentajeRecaudo' fill='#fafa0c' color='black'>
+                <LabelList dataKey='porcentajeRecaudo' position='top' fill='#7d7a79' formatter={customTooltipFormatter} />
+              </Bar>
 
-            <Line name='Avance del mes' dataKey='porcentajeMes' stroke='#fd0001'>
-              <LabelList dataKey='porcentajeMes' position='insideBottomLeft' fill='#7d7a79' formatter={customTooltipFormatter} />
-            </Line>
-          </ComposedChart>
-        </ResponsiveContainer>
-      </div>
-    </div>
+              <Line name='Avance del mes' dataKey='porcentajeMes' stroke='#fd0001'>
+                <LabelList dataKey='porcentajeMes' position='insideBottomLeft' fill='#7d7a79' formatter={customTooltipFormatter} />
+              </Line>
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
+      </section>
+    </>
   )
 }
 
