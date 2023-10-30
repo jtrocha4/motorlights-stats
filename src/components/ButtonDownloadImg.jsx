f9a7b7d57be761c5347cd31811893820f5912c7c
jtrocha4 <jtrocha04@gmail.com>
2023-10-30 17:15:00 -0500

    update(components): actualizacion de la funcion handleDownload para capturar la grafica completa sin importar el tamaño de la pantalla

--- src/components/ButtonDownloadImg.jsx
+++ src/components/ButtonDownloadImg.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
-import domToImage from 'dom-to-image'
+import html2canvas from 'html2canvas'
 import { saveAs } from 'file-saver'
 
-const ButtonDownloadImg = ({ title, background = 'primary', containerRef, date }) => {
+const ButtonDownloadImg = ({ title, background = 'primary', containerRef, date, screenSize }) => {
   const saveBlob = (blob) => {
     saveAs(blob, `Como Vamos ${date} .png`)
   }
 
   const handleDownload = () => {
     const node = containerRef.current
-
-    const options = {
-      quality: 1
-      // style: {
-      //   background: 'white'
-      // }
+    const originalStyles = {
+      width: node.style.width,
+      height: node.style.height,
+      position: node.style.position,
+      top: node.style.top,
+      left: node.style.left
     }
 
-    domToImage.toBlob(node, options)
-      .then(saveBlob)
+    node.style.width = `${screenSize}px`
+    // node.style.height = '100%'
+    node.style.position = 'absolute'
+    node.style.top = '0'
+    node.style.left = '0'
+
+    const options = {
+      scale: 3,
+      backgroundColor: 'white'
+    }
+
+    html2canvas(node, options).then((canvas) => {
+      const blob = canvas.toDataURL('image/png')
+      saveBlob(blob)
+
+      node.style.width = originalStyles.width
+      node.style.height = originalStyles.height
+      node.style.position = originalStyles.position
+      node.style.top = originalStyles.top
+      node.style.left = originalStyles.left
+    }).catch((error) => {
+      console.error('Error al capturar la imagen:', error)
+    })
   }
 
   return (
