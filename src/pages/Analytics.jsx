3cf5ee6798defa8558219c55f7b133a2507d2781
jtrocha4 <jtrocha04@gmail.com>
2023-11-02 18:16:06 -0500

    update(pages): implementacion de SaleItemContext, componente ButtonDownloadAnalyticsCostAndSale y TableAnalyticsCostAndSales

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
-import TableAnalytics from '../components/TableAnalytics'
+// import TableAnalytics from '../components/TableAnalytics'
 import { SaleItemContext } from '../components/context/saleItem'
 import { DataContext } from '../components/context/data'
 import ButtonDownloadAnalytics from '../components/ButtonDownloadAnalytics'
+import TableAnalyticsCostAndSales from '../components/TableAnalyticsCostAndSales'
+import ButtonDownloadAnalyticsCostAndSale from '../components/ButtonDownloadAnalyticsCostAndSale'
 
 const Analytics = ({ convertExcelDateToReadable, currencyFormat }) => {
   const { dateExcel } = useContext(DataContext)
-  const { sellerSalesData } = useContext(SaleItemContext)
+  const { sellerSalesData, costAndSalesData } = useContext(SaleItemContext)
 
   return (
     <div className='flex'>
@@ -15,11 +17,13 @@ const Analytics = ({ convertExcelDateToReadable, currencyFormat }) => {
         <p>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</p>
         <section className='mt-4'>
           <div className='d-grid gap-2 d-md-flex justify-content-md-end mb-2'>
-            <ButtonDownloadAnalytics title='Descargar informe' background='success' sellerSalesData={sellerSalesData} />
+            <ButtonDownloadAnalyticsCostAndSale title='Descargar Informe' background='success' costAndSalesData={costAndSalesData} />
+            <ButtonDownloadAnalytics title='Descargar Informe Macro' background='success' sellerSalesData={sellerSalesData} />
           </div>
         </section>
         <section className='mt-2 table-responsive'>
-          <TableAnalytics sellerSalesData={sellerSalesData} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} />
+          {/* <TableAnalytics sellerSalesData={sellerSalesData} convertExcelDateToReadable={convertExcelDateToReadable} currencyFormat={currencyFormat} /> */}
+          <TableAnalyticsCostAndSales currencyFormat={currencyFormat} />
         </section>
       </div>
     </div>
