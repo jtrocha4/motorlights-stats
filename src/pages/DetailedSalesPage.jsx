23514d6d04d52ad61c51d6462320e36e365c219d
jtrocha4 <jtrocha04@gmail.com>
2023-11-04 16:03:07 -0500

    update(pages): DetailedSalesPage

--- src/pages/DetailedSalesPage.jsx
+++ src/pages/DetailedSalesPage.jsx
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react'
 import { DataContext } from '../components/context/data'
+import ButtonDownloadAnalytics from '../components/ButtonDownloadAnalytics'
+import { SaleItemContext } from '../components/context/saleItem'
 
 const DetailedSalesPage = () => {
   const { dateExcel } = useContext(DataContext)
+  const { sellerSalesData } = useContext(SaleItemContext)
+
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Informe Detallado Ventas</h2>
         <span>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</span>
+        <section className='button-group mt-4'>
+          <ButtonDownloadAnalytics title='Descargar Informe Macro' background='success' sellerSalesData={sellerSalesData} />
+        </section>
       </div>
     </div>
   )
