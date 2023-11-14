1dd2c19122115d5e541c90af82fb97c42996b3fe
jtrocha4 <jtrocha04@gmail.com>
2023-11-14 14:12:18 -0500

    update(pages): implementacion del componente ButtonDownloadSellerSalesByMunicipality

--- src/pages/DetailedSalesPage.jsx
+++ src/pages/DetailedSalesPage.jsx
@@ -4,8 +4,9 @@ import ButtonDownloadAnalytics from '../components/ButtonsDownload/ButtonDownloa
 import { SaleItemContext } from '../context/saleItem'
 import { DataExcelContext } from '../context/dataExcel'
 import ButtonDownloadSaleByMunicipalities from '../components/buttonsDownload/ButtonDownloadSaleByMunicipalities'
+import ButtonDownloadSellerSalesByMunicipality from '../components/buttonsDownload/ButtonDownloadSellerSalesByMunicipality'
 
-const DetailedSalesPage = () => {
+const DetailedSalesPage = ({ splitName }) => {
   const { dateExcel } = useContext(DataExcelContext)
   const { sellerSalesData } = useContext(SaleItemContext)
 
@@ -15,7 +16,8 @@ const DetailedSalesPage = () => {
         <h2>Informe Detallado Ventas</h2>
         <span>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</span>
         <section className='button-group mt-4'>
-          <ButtonDownloadSaleByMunicipalities title='Descargar Ventas por Municipios' sellerSalesData={sellerSalesData} />
+          <ButtonDownloadSaleByMunicipalities title='Descargar Ventas por Municipio' sellerSalesData={sellerSalesData} />
+          <ButtonDownloadSellerSalesByMunicipality title='Descargar Ventas Vendedor por Municipio' sellerSalesData={sellerSalesData} splitName={splitName} />
         </section>
         <hr />
         <section className='button-group mt-4'>
