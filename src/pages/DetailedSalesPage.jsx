92bc5a2252f10674a3f9dc1657bd62e908aedb87
jtrocha4 <jtrocha04@gmail.com>
2023-11-30 13:49:34 -0500

    update(pages): filtrado del flete mercancia en el sellerSalesData. Componente DetailedSalesPage

--- src/pages/DetailedSalesPage.jsx
+++ src/pages/DetailedSalesPage.jsx
@@ -12,21 +12,24 @@ const DetailedSalesPage = ({ splitName }) => {
   const { dateExcel } = useContext(DataExcelContext)
   const { sellerSalesData } = useContext(SaleItemContext)
 
+  const sellerSalesProcessedData = sellerSalesData
+    .filter(row => !row.producto.startsWith('Flete'))
+
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Informe Detallado Ventas</h2>
         <span>{(dateExcel.dia !== undefined && dateExcel.mes !== undefined) ? (`Fecha de la última actualización: ${dateExcel.dia} de ${dateExcel.mes} de ${dateExcel.año}`) : ('Aun no hay informes cargados, por favor cargue los informes para tener una visualizacion de la data')}</span>
         <section className='button-group mt-4'>
-          <ButtonDownloadSalesByMunicipality title='Descargar Ventas por Municipio' sellerSalesData={sellerSalesData} />
-          <ButtonDownloadSellerSalesByMunicipality title='Descargar Ventas Vendedor por Municipio' sellerSalesData={sellerSalesData} splitName={splitName} />
-          <ButtonDownloadSalesMonthSellerByMunicipality title='Descargar Ventas Mes Vendedor por Municipio' sellerSalesData={sellerSalesData} splitName={splitName} />
-          <ButtonDownloadSalesCustomerSellerByMunicipality title='Descargar Ventas Cliente Vendedor por Municipio' sellerSalesData={sellerSalesData} splitName={splitName} />
-          <ButtonDownloadSalesMonthSellerCustomerByMunicipality title='Descargar Ventas Mes Vendedor Cliente por Municipio' sellerSalesData={sellerSalesData} splitName={splitName} />
+          <ButtonDownloadSalesByMunicipality title='Descargar Ventas por Municipio' sellerSalesData={sellerSalesProcessedData} />
+          <ButtonDownloadSellerSalesByMunicipality title='Descargar Ventas Vendedor por Municipio' sellerSalesData={sellerSalesProcessedData} splitName={splitName} />
+          <ButtonDownloadSalesMonthSellerByMunicipality title='Descargar Ventas Mes Vendedor por Municipio' sellerSalesData={sellerSalesProcessedData} splitName={splitName} />
+          <ButtonDownloadSalesCustomerSellerByMunicipality title='Descargar Ventas Cliente Vendedor por Municipio' sellerSalesData={sellerSalesProcessedData} splitName={splitName} />
+          <ButtonDownloadSalesMonthSellerCustomerByMunicipality title='Descargar Ventas Mes Vendedor Cliente por Municipio' sellerSalesData={sellerSalesProcessedData} splitName={splitName} />
         </section>
         <hr />
         <section className='button-group mt-4'>
-          <ButtonDownloadAnalytics title='Descargar Informe Macro' background='success' sellerSalesData={sellerSalesData} />
+          <ButtonDownloadAnalytics title='Descargar Informe Macro' background='success' sellerSalesData={sellerSalesProcessedData} />
         </section>
       </div>
     </div>
