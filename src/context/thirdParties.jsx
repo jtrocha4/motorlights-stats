d60bfff3329ba1f2411a3811e872b44db0d59b91
jtrocha4 <jtrocha04@gmail.com>
2023-12-14 14:10:30 -0500

    refactor(context): cambio de nombre del estado customerData a customer. ThirdParties

--- src/context/thirdParties.jsx
+++ src/context/thirdParties.jsx
@@ -4,14 +4,14 @@ export const ThirdPartiesContext = createContext()
 
 export const ThirdPartiesProvider = ({ children }) => {
   const [thirdPartiesData, setThirdPartiesData] = useState([])
-  const [customerData, setCustomerData] = useState([])
+  const [customer, setCustomer] = useState([])
 
   return (
     <ThirdPartiesContext.Provider value={{
       thirdPartiesData,
       setThirdPartiesData,
-      customerData,
-      setCustomerData
+      customer,
+      setCustomer
     }}
     >
       {children}
