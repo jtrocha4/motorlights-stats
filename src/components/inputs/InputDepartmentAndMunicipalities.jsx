9b09598ad810bf8dd72fbb5aefe0c13d17c11700
jtrocha4 <jtrocha04@gmail.com>
2023-12-21 12:14:42 -0500

    update(components): implementacion del metodo postMunicipalityToApi. InputDepartmentAndMunicipalities

--- src/components/inputs/InputDepartmentAndMunicipalities.jsx
+++ src/components/inputs/InputDepartmentAndMunicipalities.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { departAndMunicFileToModel } from '../../mappers'
 import * as XLSX from 'xlsx'
 
-const InputDepartmentAndMunicipalities = ({ label }) => {
+const InputDepartmentAndMunicipalities = ({ label, capitalizeWords, removeExtraSpaces, department, postMunicipalityToApi }) => {
   const [excelDeptAndMunic, setExcelDeptAndMunic] = useState([])
 
   const handleReadDepartmentAndMunicipalities = (event) => {
@@ -35,12 +35,38 @@ const InputDepartmentAndMunicipalities = ({ label }) => {
   const headersDeptAndMunic = excelDeptAndMunic[0]
   const rowsDeptAndMunic = excelDeptAndMunic.slice(1)
   const formattedDataDeptAndMunic = formatDataDeptAndMunic(headersDeptAndMunic, rowsDeptAndMunic)
-  console.log(formattedDataDeptAndMunic)
+
+  const leakedData = []
+
+  formattedDataDeptAndMunic.forEach(el => {
+    const { municipio, departamento } = el
+
+    department.forEach(dept => {
+      if (dept.nombre === removeExtraSpaces(capitalizeWords(departamento))) {
+        leakedData.push({
+          nombre: removeExtraSpaces(capitalizeWords(municipio)),
+          idDepartamento: dept.id
+        })
+      }
+    })
+  })
+
+  const handleUpload = async () => {
+    try {
+      for (const key in leakedData) {
+        await postMunicipalityToApi(leakedData[key])
+      }
+      console.log('Papi todo good good')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
       <label className='form-label'>{label}</label>
       <input className='form-control' type='file' accept='.xls , .xlsx' onChange={handleReadDepartmentAndMunicipalities} />
+      <button type='button' className='btn btn-primary' onClick={handleUpload}>Subir</button>
     </>
   )
 }
