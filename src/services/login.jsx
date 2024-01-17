7c6a796f2c582a54ff48c6104f43a1cee7f6f040
jtrocha4 <jtrocha04@gmail.com>
2024-01-17 11:12:15 -0500

    update(services): login

--- src/services/login.jsx
+++ src/services/login.jsx
@@ -2,8 +2,15 @@ import axios from 'axios'
 const baseUrl = import.meta.env.VITE_URL_MONGODB
 
 const login = async (credential) => {
-  const response = await axios.post(`${baseUrl}/api/login`, credential)
-  return response.data
+  try {
+    const response = await axios.post(`${baseUrl}/api/login`, credential)
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.error('Error en la autentificacion. Usuario o contraseña no válidos')
+    throw error
+  }
 }
 
 export { login }
