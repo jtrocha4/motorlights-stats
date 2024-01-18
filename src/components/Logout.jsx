45999dddb9c7aa4b313de59517ba41963e22b765
jtrocha4 <jtrocha04@gmail.com>
2024-01-18 18:23:44 -0500

    update(components): Logout

---
+++ src/components/Logout.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/user'
+
+const Logout = ({ background = 'btn btn-outline-light', title }) => {
+  const { setUser } = useContext(UserContext)
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedApp')
+  }
+
+  return (
+    <>
+      <button type='button' className={`${background}`} onClick={handleLogout}>{title} <i className='fa-solid fa-right-from-bracket' /></button>
+    </>
+  )
+}
+
+export default Logout
