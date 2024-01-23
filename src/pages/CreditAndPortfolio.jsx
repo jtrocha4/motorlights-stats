a97d13a2231b40b0941a97fa4ee634e09371dca0
jtrocha4 <jtrocha04@gmail.com>
2024-01-23 12:24:57 -0500

    feat(pages): CreditAndPortfolio

---
+++ src/pages/CreditAndPortfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreditAndPortfolio = () => {
+  return (
+    <div className='flex'>
+      <div className='container-fluid'>
+        <h2>Informe de Credito y Cartera</h2>
+      </div>
+    </div>
+  )
+}
+
+export default CreditAndPortfolio
