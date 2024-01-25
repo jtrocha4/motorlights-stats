f2b229002bfb1e0dae4ee58eb90e8965caa13075
jtrocha4 <jtrocha04@gmail.com>
2024-01-25 12:39:47 -0500

    update(pages): CreditAndPortfolio

--- src/pages/CreditAndPortfolio.jsx
+++ src/pages/CreditAndPortfolio.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import ButtonDownloadPortfolio from '../components/buttonsDownload/ButtonDownloadPortfolio'
 
 const CreditAndPortfolio = () => {
   return (
     <div className='flex'>
       <div className='container-fluid'>
         <h2>Informe de Credito y Cartera</h2>
+        <section className='button-group mt-4'>
+          <ButtonDownloadPortfolio title='Descargar Inf. Comportamiento de Pago' />
+        </section>
       </div>
     </div>
   )
